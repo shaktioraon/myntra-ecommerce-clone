25afe516aa25fcec4bf58ac65db1c1982928f516
shakti oraon <shaktioraon314@gmail.com>
2026-08-11 12:44:17 +0530

    Fix FetchItems import case

--- myntra-react-clone/src/routes/App.jsx
+++ myntra-react-clone/src/routes/App.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import FetchItems from "../components/fetchItems";
+import FetchItems from "../components/FetchItems.jsx";
 import { useSelector } from "react-redux";
 import LoadingSpinner from "../components/LoadingSpinner";
 
