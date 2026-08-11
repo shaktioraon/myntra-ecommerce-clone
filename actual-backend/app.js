f09ee1b32a6f37cb72f4768e2897895f54bd7160
shakti oraon <shaktioraon314@gmail.com>
2026-08-11 10:52:14 +0530

    Update backend port for deployment

--- actual-backend/app.js
+++ actual-backend/app.js
@@ -38,4 +38,8 @@ app.post('/items', async (req, res) => {
   res.status(201).json({ message: 'Stored new item.', item: newItem });
 });
 
-app.listen(8080);
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
